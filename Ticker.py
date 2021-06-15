7122cf1191cf2751d8049ec5045cedde1032d929
Trevor Smith <trevor.smith@is4s.com>
2021-06-15 14:42:01 -0500

    fix timer issue

--- Ticker.py
+++ Ticker.py
@@ -1,11 +1,12 @@
 from tkinter import *
+from numpy import double
 import yfinance as yf
 import sched, time
 
 tickerArray = ['MSFT','DIS','GOOG','GME','AMC','BB','CLF']
 tk = Tk()
 tk.title("Stock Tickers for APES")
-tk.attributes('-zoomed', True)
+tk.attributes('-fullscreen', False)
 frame = Frame(tk)
 frame.pack
 
@@ -39,36 +40,38 @@ class Dashboard:
         tickerDiff.grid(row = index*2+1, column = 0, sticky=E)
 
     def set_label_color(self,labelPrice,labelDiff):
-        if int(labelDiff["text"]) < 0:
-            labelPrice.config(fg="red")
-            labelDiff.config(fg="red")
+        if float(labelDiff["text"]) < 0:
+            labelPrice.configure(fg="red")
+            labelDiff.configure(fg="red")
         else:
-            labelPrice.config(fg="green")
-            labelDiff.config(fg="green")
+            labelPrice.configure(fg="green")
+            labelDiff.configure(fg="green")
 
-    def update_every_minute(self,tickerIn):
-        self.update_ticker_by_row(tickerIn)
+    def update_every_minute(self,tickerArrayIn):
+        self.update_ticker_by_row()
     
     def enumerate_ticker(self,tickerIn):
         for index,ticker in enumerate(tickerIn):
             self.add_ticker(index,ticker)
 
-    def update_ticker_by_row(self,tickerIn):
-        for index,ticker in enumerate(tickerIn):
+    def update_ticker_by_row(self):
+        for index,ticker in enumerate(tickerArray):
             tickerName = tk.grid_slaves(row=index*2,column=0)[0]
             tickerPrice = tk.grid_slaves(row=index*2,column=1)[0]
             tickerDiff = tk.grid_slaves(row=index*2+1)[0]
             self.update_ticker_item(tickerName,tickerPrice,tickerDiff)
+        tk.after(20000,self.update_ticker_by_row)
     
     def update_ticker_item(self,name,price,diff):
-        stock = yf.Ticker(name)
+        stock = yf.Ticker(name["text"])
 
         openPrice = stock.history(period='1d')['Open'][0]
         currPrice = stock.history(period='1d')['Close'][0]
         difference = currPrice - openPrice
         price.configure(text=currPrice)
         diff.configure(text=difference)
-
+        
+        self.set_label_color(price,diff)
         
 if __name__ == '__main__':
     dash = Dashboard()
